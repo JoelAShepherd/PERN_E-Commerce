02e03f36ce5101ec9c820d2fe06ba99a0d66e7ad
JoelAShepherd <joel125shepherd@gmail.com>
2021-08-23 11:28:37 +0100

    basic file structure setup

--- Client/app.js
+++ Client/app.js
@@ -1,6 +1,11 @@
 
 function App () {
     return (
-        
+        <div className="App">
+            <h1>Title</h1>
+            <p>Here we go!</p>
+        </div>
     )
 }
+
+export default App;
