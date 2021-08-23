bd4ba2cb0ede1207a778753b645686446fd3d074
JoelAShepherd <joel125shepherd@gmail.com>
2021-08-23 12:05:26 +0100

    dev server up and running

--- Client/app.js
+++ Client/app.js
@@ -1,9 +1,11 @@
+import React from 'react';
 
 function App () {
     return (
         <div className="App">
             <h1>Title</h1>
             <p>Here we go!</p>
+            <p>Here we go again!</p>
         </div>
     )
 }
