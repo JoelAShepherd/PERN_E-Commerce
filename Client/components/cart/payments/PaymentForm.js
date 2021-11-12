2b8ba08f24899708e59cb2682679e6f38c5af0b6
JoelAShepherd <joel125shepherd@gmail.com>
2021-11-12 13:25:57 +0000

    fixed payment bug

--- Client/components/cart/payments/PaymentForm.js
+++ Client/components/cart/payments/PaymentForm.js
@@ -32,9 +32,7 @@ export default function PaymentForm() {
     const stripe = useStripe();
     const elements = useElements();
     const cartTotal = useSelector(selectCartTotalPrice)
-    console.log("cart total  ", cartTotal)
     const totalToPay = ((cartTotal.toFixed(2))*100).toFixed();
-    console.log(" ***** Total to pay ", totalToPay)
     const order = useSelector(selectCartOrder);
     const paymentAndOrderCompleted = useSelector(selectPaymentSuccess)
 
